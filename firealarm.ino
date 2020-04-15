0bb570da3a217423c3a54f633d243854dc89320f
Koustubh <36124868+babban-techie@users.noreply.github.com>
2020-04-15 21:00:47 +0530

    Add files via upload

---
+++ firealarm.ino
@@ -0,0 +1,32 @@
+#include<Servo.h>
+
+int firepin = A0;
+int fire = 0;
+int led = 13;
+int pos = 0;
+
+Servo myservo;
+
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(115200);
+  myservo.attach(9);
+}
+
+void loop() {
+  fire = analogRead(A0);
+  if (fire<100)
+  {
+    Serial.println(fire);
+    digitalWrite(led,HIGH);
+    myservo.write(180);              // tell servo to go to position in variable 'pos'
+    delay(2000);                       // waits 15ms for the servo to reach the position
+  }
+  else
+  {
+    Serial.println(fire);
+    digitalWrite(led,LOW);
+    myservo.write(0);              // tell servo to go to position in variable 'pos'
+    delay(2000);              
+  }
+}
